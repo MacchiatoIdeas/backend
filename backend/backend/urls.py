042afb7841423327dbbed1fe1fed684a0befc966
Autopawn <franciscojacb@gmail.com>
2017-08-07 05:32:03 -0400

    Added REST API authentication URLS.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^material/', include('material.urls', namespace='material')),
+]
+
+# Add authentication for the Browsable API
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+        namespace='rest_framework')),
 ]
