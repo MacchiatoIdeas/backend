2bcbcf2b02036c70a580e384fb15a710f35b5a44
Rodolfo Castillo Mateluna <rodolfocastillomateluna@gmail.com>
2017-08-15 16:59:50 -0300

    Add MEDIA_ROOT

--- backend/settings.py
+++ backend/settings.py
@@ -129,5 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Allow all origins
 CORS_ORIGIN_ALLOW_ALL = True
