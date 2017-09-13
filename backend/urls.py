b69e70ecbd9239732c68a5c1d92e82bf7f9b06ea
Autopawn <franciscojacb@gmail.com>
2017-09-13 15:39:56 -0300

    Deleted foo bar index placeholder and added exercises to index router.

--- backend/urls.py
+++ backend/urls.py
@@ -9,14 +9,15 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'users', uviews.UserViewSet)
 router.register(r'groups', uviews.GroupViewSet)
+router.register(r'exercises', uviews.GroupViewSet)
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^material/', include('material.urls', namespace='material')),
     url(r'^exercises/', include('exercises.urls', namespace='exercises')),
     url(r'^', include(router.urls)),
-    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider'))
+    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
 
 # Add authentication for the Browsable API
--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,1 @@
 from django.http import JsonResponse
-from django.views.decorators.csrf import ensure_csrf_cookie
-
-
-@ensure_csrf_cookie
-def index(request):
-	return JsonResponse({'foo': 'bar'})
