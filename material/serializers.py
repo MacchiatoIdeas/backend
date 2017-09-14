1ce6390d86fae6351e3ac7d83e2293969f22d8b5
Rodolfo Castillo Mateluna <rodolfocastillomateluna@gmail.com>
2017-09-13 22:36:52 -0300

    Fix declarations

--- material/serializers.py
+++ material/serializers.py
@@ -5,80 +5,82 @@ from .models import *
 
 
 class ContentListSerializer(serializers.ModelSerializer):
-    text = serializers.ReadOnlyField(source='abstract')
-    author = TeacherSerializer()
+	text = serializers.ReadOnlyField(source='abstract')
+	author = TeacherSerializer()
 
-    class Meta:
-        model = Content
-        fields = ('id', 'unit', 'summary', 'text', 'html_text', 'author')
-
-
-class UnitSerializer(serializers.ModelSerializer):
-    contents = ContentListSerializer(many=True, read_only=True)
-    subject = SubjectSerializer(read_only=True)
-
-    class Meta:
-        model = Unit
-        fields = ('id', 'subject', 'name', 'academic_level', 'contents')
-
-
-class UnitListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Unit
-        fields = ('id', 'subject', 'name', 'academic_level')
+	class Meta:
+		model = Content
+		fields = ('id', 'unit', 'summary', 'text', 'html_text', 'author')
 
 
 class SubjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subject
-        fields = ('id', 'name', 'color', 'thumbnail')
+	class Meta:
+		model = Subject
+		fields = ('id', 'name', 'color', 'thumbnail')
 
 
 class SubjectListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subject
-        fields = ('id', 'name', 'color', 'thumbnail')
+	class Meta:
+		model = Subject
+		fields = ('id', 'name', 'color', 'thumbnail')
+
+
+class UnitSerializer(serializers.ModelSerializer):
+	contents = ContentListSerializer(many=True, read_only=True)
+	subject = SubjectSerializer(read_only=True)
+
+	class Meta:
+		model = Unit
+		fields = ('id', 'subject', 'name', 'academic_level', 'contents')
+
+
+class UnitListSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Unit
+		fields = ('id', 'subject', 'name', 'academic_level')
 
 
 class SubjectRetrieveSerializer(serializers.ModelSerializer):
-    units = UnitListSerializer(many=True, read_only=True)
+	units = UnitListSerializer(many=True, read_only=True)
 
-    class Meta:
-        model = Subject
-        fields = ('id', 'name', 'units', 'color', 'thumbnail')
+	class Meta:
+		model = Subject
+		fields = ('id', 'name', 'units', 'color', 'thumbnail')
 
 
 class ContentSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.user.username')
-    # TODO: Change user name for the proper full name of a teacher.
+	author = serializers.ReadOnlyField(source='author.user.username')
 
-    class Meta:
-        model = Content
-        fields = ('id', 'unit', 'subtitle', 'summary', 'text', 'html_text', 'author')
+	# TODO: Change user name for the proper full name of a teacher.
+
+	class Meta:
+		model = Content
+		fields = ('id', 'unit', 'subtitle', 'summary', 'text', 'html_text', 'author')
 
 
 class ContentRetrieveSerializer(serializers.ModelSerializer):
-    author = TeacherSerializer()
-    unit = UnitSerializer()
+	author = TeacherSerializer()
+	unit = UnitSerializer()
 
-    class Meta:
-        model = Content
-        fields = ('id', 'unit', 'subtitle', 'summary', 'text', 'html_text', 'author')
+	class Meta:
+		model = Content
+		fields = ('id', 'unit', 'subtitle', 'summary', 'text', 'html_text', 'author')
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    user = GenericUserSerializer(read_only=True)
-    # TODO: Should the content be retrieved?
-    #content = ContentSerializer()
+	user = GenericUserSerializer(read_only=True)
 
-    class Meta:
-        model = Comment
-        fields = '__all__'
+	# TODO: Should the content be retrieved?
+	# content = ContentSerializer()
+
+	class Meta:
+		model = Comment
+		fields = '__all__'
 
 
 class FeedbackCommentSerializer(serializers.ModelSerializer):
-    user = GenericUserSerializer(read_only=True)
+	user = GenericUserSerializer(read_only=True)
 
-    class Meta:
-        model = FeedbackComment
-        fields = '__all__'
+	class Meta:
+		model = FeedbackComment
+		fields = '__all__'
