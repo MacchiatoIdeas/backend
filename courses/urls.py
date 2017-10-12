83a1a423734d0a8348c7d096a2547e50abbc712d
Autopawn <franciscojacb@gmail.com>
2017-10-12 08:31:43 -0300

    Fixed router endpoints of courses.

--- courses/urls.py
+++ courses/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'^courses', views.CourseViewSet)
-router.register(r'^courselinks', views.CourseLinkViewSet)
+router.register(r'courses', views.CourseViewSet)
+router.register(r'courselinks', views.CourseLinkViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
