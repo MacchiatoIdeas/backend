04bcec5fe8491eddd88fa23fee1d4dc409384822
alexmussor <alexmussor@gmail.com>
2017-10-11 22:28:11 -0300

    Users url fix

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,8 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'courses', views.CourseViewSet)
+router.register(r'^courses', views.CourseViewSet)
+router.register(r'', views.UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
